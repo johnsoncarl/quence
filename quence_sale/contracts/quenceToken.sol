485414fbb0c3b0dde944f2495f1b4488e53f5c79
Aman Pandey <man22invisible@gmail.com>
2018-10-27 12:39:43 +0530

    error removed by assigning _initialSupply to default balance of the admin in quenceToken.sol

--- quence_sale/contracts/quenceToken.sol
+++ quence_sale/contracts/quenceToken.sol
@@ -5,9 +5,10 @@ contract quenceToken{
 	uint256 public totalSupply;
 
 	// mapping balaceOf
-	mapping(address => uint256) public balaceOf;
+	mapping(address => uint256) public balanceOf;
 
 	function quenceToken (uint256 _initialSupply) public{
+		balanceOf[msg.sender] = _initialSupply;
 		totalSupply = _initialSupply;	//	by ERC20 standards
 	}
 }
