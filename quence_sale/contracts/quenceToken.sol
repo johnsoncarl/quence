855aff3589c0393300076d2a4cf035bbf3ed3047
Aman Pandey <man22invisible@gmail.com>
2018-10-27 12:22:22 +0530

    Added totalSupply to be passed as an argument

--- quence_sale/contracts/quenceToken.sol
+++ quence_sale/contracts/quenceToken.sol
@@ -4,7 +4,7 @@ contract quenceToken{
 	// creating constructor
 	uint256 public totalSupply;
 
-	function quenceToken () public{
-		totalSupply = 1000000;	//	by ERC20 standards
+	function quenceToken (uint256 _initialSupply) public{
+		totalSupply = _initialSupply;	//	by ERC20 standards
 	}
 }
--- quence_sale/migrations/2_deploy_contracts.js
+++ quence_sale/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var quenceToken = artifacts.require("./quenceToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(quenceToken);
+  deployer.deploy(quenceToken, 1000000);
 };
