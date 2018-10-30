2fc3617ab9c674549828c8ce61505bda4e5c74e4
Aman Pandey <man22invisible@gmail.com>
2018-10-30 13:57:39 +0530

    The deployment of quenceTokenSale was not done properly. Forgot to make changes into migrations.(Now Done)

--- quence_sale/migrations/2_deploy_contracts.js
+++ quence_sale/migrations/2_deploy_contracts.js
@@ -3,4 +3,5 @@ var quenceTokenSale = artifacts.require("./quenceTokenSale.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(quenceToken, 1000000); // this 1000000 passes through the constructor of the quenceToken.sol
+  deployer.deploy(quenceTokenSale);
 };
--- quence_sale/test/quenceTokenSale.js
+++ quence_sale/test/quenceTokenSale.js
@@ -1,1 +1,18 @@
-quenceTokenSale.js
+var quenceTokenSale = artifacts.require("./quenceTokenSale.sol");
+
+
+contract('quenceTokenSale', function(accounts){
+
+	var tokenSaleInstance;
+
+	it('To initialize the contact with the correct values', function(){
+
+		return quenceTokenSale.deployed().then(function(instance){
+
+			tokenSaleInstance = instance;
+			return tokenSaleInstance.address;
+		}).then(function(address){
+			assert.notEqual(address, 0x0, 'has some contract address')
+		})
+	})
+})
