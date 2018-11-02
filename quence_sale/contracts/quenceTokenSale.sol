9aa20234ca7a16b26785f52c1be97250efb671a4
Aman Pandey <man22invisible@gmail.com>
2018-11-02 18:09:50 +0530

    initialization of the tokenPrice completed

--- quence_sale/contracts/quenceTokenSale.sol
+++ quence_sale/contracts/quenceTokenSale.sol
@@ -6,10 +6,15 @@ contract quenceTokenSale{
 
 	address admin;
 	quenceToken public tokenContract;
+	uint256 public tokenPrice;
 
-	function quenceTokenSale(quenceToken _tokenContract) public {
+
+	function quenceTokenSale(quenceToken _tokenContract, uint256 _tokenPrice) public {
 			admin = msg.sender;
 			tokenContract = _tokenContract;
+			tokenPrice = _tokenPrice;
+
+
 	}
 
 }
--- quence_sale/migrations/2_deploy_contracts.js
+++ quence_sale/migrations/2_deploy_contracts.js
@@ -3,6 +3,8 @@ var quenceTokenSale = artifacts.require("./quenceTokenSale.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(quenceToken, 1000000).then(function(){
-  	return deployer.deploy(quenceTokenSale, quenceToken.address);
+  	
+  	var tokenPrice = 100000000000000; // the price is in WEI equal to 0.0001 ETH
+  	return deployer.deploy(quenceTokenSale, quenceToken.address, tokenPrice);
   }); // this 1000000 passes through the constructor of the quenceToken.sol
 };
